0d23d1e7781dd332fe5094a3a95fd5118a857448
Fuad Jemal <fuad47722@gmail.com>
2025-04-30 10:37:25 +0300

    Add getDetailCours functionality to student router

--- server/Route/studentRouter.js
+++ server/Route/studentRouter.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { studentSignUp } = require('../controller/student/sign.up')
 const { studentLogin } = require('../controller/student/login')
 const { getCoursesOnePerCategory } = require('../controller/student/getCours')
+const { getDetailCours } = require('../controller/student/get.detail.course')
 
 const router = express.Router()
 
@@ -9,6 +10,7 @@ const router = express.Router()
 router.post('/student-sign-up', studentSignUp)
 router.post('/login', studentLogin)
 router.get('/get-cours', getCoursesOnePerCategory)
+router.get('/get-detail-course/:id', getDetailCours)
 
 
 module.exports = router
