b8e6cdf251ce5adae6de57ab42aa606fd1931aad
Fuad Jemal <fuad47722@gmail.com>
2025-05-05 06:54:59 +0300

    Remove getDetailCours endpoint and update routing for course details

--- server/Route/studentRouter.js
+++ server/Route/studentRouter.js
@@ -2,7 +2,6 @@ const express = require('express')
 const { studentSignUp } = require('../controller/student/sign.up')
 const { studentLogin } = require('../controller/student/login')
 const { getCoursesByDepartment } = require('../controller/student/getCours')
-const { getDetailCours } = require('../controller/student/get.detail.course')
 const { giveComment } = require('../controller/student/give.comment')
 const { updateStudent } = require('../controller/supperAdmin/edit.student')
 const { askQuestion } = require('../controller/student/askQuation')
@@ -29,8 +28,7 @@ router.put('/edit-profile/:id', updateStudent)
 
 // get
 router.get('/count-answer', getStudentAnswerCount)   //you accept  answer_count
-router.get('/get-cours/:id', getCoursesByDepartment)
-router.get('/get-detail-course/:id', getDetailCours)
+router.get('/get-cours/:id', getCoursesByDepartment) //to get detail about course
 router.get('/get-quation-answer/:id', getStudentCourseQA)
 router.get('/get-all-course', getAllCourses)
 router.get('/get-course-by-year/:year', getCoursesByYear)
